af10a9e8f1793d53fc24e4d7e169f0ded4613ab6
hadisak <hadisehmanouchehri@gmail.com>
2026-09-06 02:37:47 +0330

    feat: add file upload and storage

--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
 	"go-practice/STORAGE/internal/auth"
 	"go-practice/STORAGE/internal/auth/user"
 	"go-practice/STORAGE/internal/database"
+	"go-practice/STORAGE/internal/storage"
 
 	"github.com/joho/godotenv"
 )
@@ -27,13 +28,26 @@ func main() {
 	userRepository := user.NewUserRepository(db)
 
 	authService := auth.NewAuthService(userRepository)
-
 	authController := auth.NewAuthController(authService)
+
+	storageRepository := storage.NewStorageRepository(db)
+	storageService := storage.NewStorageService(storageRepository)
+	storageController := storage.NewStorageController(storageService)
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /signup", authController.SignUp)
 	mux.HandleFunc("POST /signin", authController.SignIn)
+
+	mux.Handle(
+		"POST /upload",
+		auth.Authentication(http.HandlerFunc(storageController.Upload)),
+	)
+
+	mux.Handle(
+		"GET /files",
+		auth.Authentication(http.HandlerFunc(storageController.List)),
+	)
 
 	log.Println("Server is running on :8080")
 
