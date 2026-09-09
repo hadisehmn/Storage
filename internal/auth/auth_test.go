c68d0558dcb6f88264811750f7b6461c39e780a4
hadisak <hadisehmanouchehri@gmail.com>
2026-09-10 02:56:41 +0330

    test: add signup service tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	models "go-practice/STORAGE/internal/model"
+	"testing"
+)
+
+func TestSignUp(t *testing.T) {
+
+	tests := []struct {
+		name      string
+		req       models.SignUpRequest
+		wantError string
+	}{
+		{
+			name: "name required",
+			req: models.SignUpRequest{
+				Name:     "",
+				Email:    "ali@test.com",
+				Password: "123456",
+			},
+			wantError: "name is required",
+		},
+		{
+			name: "email required",
+			req: models.SignUpRequest{
+				Name:     "Ali",
+				Email:    "",
+				Password: "123456",
+			},
+			wantError: "email is required",
+		},
+		{
+			name: "password required",
+			req: models.SignUpRequest{
+				Name:     "Ali",
+				Email:    "ali@test.com",
+				Password: "",
+			},
+			wantError: "password is required",
+		},
+	}
+
+	for _, test := range tests {
+
+		t.Run(test.name, func(t *testing.T) {
+
+			service := &AuthService{}
+
+			_, err := service.SignUp(test.req)
+
+			if err == nil {
+				t.Fatal("expected error")
+			}
+
+			if err.Error() != test.wantError {
+				t.Errorf(
+					"expected %q, got %q",
+					test.wantError,
+					err.Error(),
+				)
+			}
+		})
+	}
+}
